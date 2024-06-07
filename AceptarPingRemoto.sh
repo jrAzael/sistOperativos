9c6623f631eaafd073203b86645d3237d36d3605
azael <azael_jr04@icloud.com>
2024-06-07 09:57:03 -0600

    Segundo Codigo

---
+++ AceptarPingRemoto.sh
@@ -0,0 +1,10 @@
+#Codigo para aceptar paquetes icmp mediante una IP
+
+# Solicitar al usuario que ingrese la IP
+echo "Ingrese la IP a aceptar por protocolo icmp"
+read BLOCK_IP
+
+# Bloquear el puerto 22 para la IP ingresada
+iptables -D INPUT -p icmp -s $BLOCK_IP -j DROP
+#iptables -A INPUT -p icmp -s $BLOCK_IP -j DROP
+echo "La direccion IP $BLOCK_IP fue desbloqueada para enviar paquetes icmp."
